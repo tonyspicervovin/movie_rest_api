73fe815f6d329462e84b589696811e7f504def80
tony <tony.spicer.covin@gmail.com>
2019-12-10 13:01:06 -0600

    switch to postgres

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -88,6 +88,12 @@ WSGI_APPLICATION = 'movieapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
