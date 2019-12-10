0047f29a848a09e747463f973ad78955ee571e16
tony <tony.spicer.covin@gmail.com>
2019-12-10 12:57:57 -0600

    switched to postgres

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 ##import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -87,12 +88,7 @@ WSGI_APPLICATION = 'movieapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
