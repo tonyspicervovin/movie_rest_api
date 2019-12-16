11b259b3b1df2daeb7933ef1af5a598b44408a6a
tony <tony.spicer.covin@gmail.com>
2019-12-16 15:39:58 -0600

    use sqlite locally

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
